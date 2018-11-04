970a3eceae723487750117af017ac38a2d4d1b60
JungMongKyo <dreamong91@gmail.com>
2018-11-04 21:10:52 +0900

    modified views.py

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Post
+from .models import Post, User
 
 
 def post_list(request):
-    posts = Post.objects.all()
-    context = {
-        'posts': posts,
-
-    }
-    return render(request, 'posts/post_list.html', context)
+    if request.method == 'POST':
+        post = Post(
+            author=User.objects.first(),
+            photo=request.FILES['photo'],
+        )
+        post.save()
+        return redirect('posts:post-list')
